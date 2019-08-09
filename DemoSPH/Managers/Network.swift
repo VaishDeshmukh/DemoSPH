dc998d9fd8dafebb07ca6c0ee2bd8923bb63f39d
Vaishnavi <>
2019-08-09 14:40:24 +0800

    Added Network related functions
    * Created Network File
    * Created JSON Object file

--- DemoSPH/Managers/Network.swift
+++ DemoSPH/Managers/Network.swift
@@ -7,3 +7,121 @@
 //
 
 import Foundation
+import Alamofire
+
+class Network {
+    //MARK:- Properties
+    static let shared = Network()
+    
+    private static let session: SessionManager = {
+        return SessionManager(configuration: URLSessionConfiguration.default)
+    }()
+    
+    //MARK:- HTTP Structure
+    enum Endpoints {
+        case getData([String : Any])
+
+        // HTTP Methods
+        internal var method: HTTPMethod {
+            switch self {
+            case .getData: return HTTPMethod.get
+            }
+        }
+        
+        //HTTP Urls
+        internal var urlString: URLConvertible {
+            switch self {
+            case .getData:
+                return "https://data.gov.sg/api/action/datastore_search?"
+            }
+        }
+        
+        // HTTP Parameters
+        internal var parameters: Parameters? {
+            var params: Parameters?
+            
+            switch self {
+            case .getData(let data):
+                params = data
+                break
+            }
+            return params as Parameters?
+        }
+        
+        // HTTP Encodings
+        internal var encoding: ParameterEncoding? {
+            var encodingType: ParameterEncoding?
+            
+            switch self {
+            case .getData:
+                encodingType = URLEncoding.default
+                break
+            }
+            return encodingType as ParameterEncoding?
+        }
+    }
+    
+    //MARK:- Request Broker
+    
+    func request(endpoint: Network.Endpoints, completion: @escaping (DataResponse<Any>) -> Void) {
+        
+        DispatchQueue.main.sync {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        }
+        
+        print("Sending request to server")
+        print("Endpoints: \(endpoint)")
+        print("HttpMethod: \(endpoint.method)")
+        print("UrlString: \(endpoint.urlString)")
+        print("Parameters: \(String(describing: endpoint.parameters))")
+        
+        let request = Network.session.request(endpoint.urlString, method: endpoint.method, parameters: endpoint.parameters, encoding: endpoint.encoding!).validate().responseJSON { response in
+            completion(response)
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+        
+        print("request: \(request)")
+    }
+    
+    func cancel() {
+        Network.session.session.getAllTasks {tasks in
+            tasks.forEach({
+                $0.cancel()
+            })
+        }
+    }
+    
+    func suspend() {
+        Network.session.session.getAllTasks {tasks in
+            tasks.forEach({
+                $0.suspend()
+            })
+        }
+    }
+    
+    func resume() {
+        Network.session.session.getAllTasks {tasks in
+            tasks.forEach({
+                $0.resume()
+            })
+        }
+    }
+    
+    func isReachable() -> Bool {
+        return NetworkReachabilityManager()?.isReachable ?? false
+    }
+    
+    func listen(completion: @escaping (Bool) -> Void) {
+        let reachabilityManager = NetworkReachabilityManager()
+        reachabilityManager?.startListening()
+        
+        reachabilityManager?.listener = { status in
+            if reachabilityManager?.isReachable ?? false {
+                completion(true)
+            } else {
+                completion(false)
+            }
+        }
+    }
+}
+
--- DemoSPH/Model/Data.swift
+++ DemoSPH/Model/Data.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import Gloss
+
+struct Data: Glossy {
+    var volume_of_mobile_data: Double!
+    var quarter: String!
+    var _id: Int!
+    
+    //MARK:- Serialization
+    
+    func toJSON() -> JSON? {
+        return jsonify([
+            "volume_of_mobile_data" ~~> self.volume_of_mobile_data,
+            "quarter" ~~> self.quarter,
+            "_id" ~~> self._id
+            ])
+    }
+    
+    //MARK:- Deserialization
+    
+    init?(json: JSON) {
+        self.volume_of_mobile_data = "volume_of_mobile_data" <~~ json
+        self.quarter = "quarter" <~~ json
+        self._id = "_id" <~~ json
+    }
+}
+
+//MARK:_ Network Call
+
+extension Data {
+    static func fetchData(data: [String: Any], completion: @escaping ([Data]?, Error?) -> Void) {
+        
+        Network.shared.request(endpoint: .getData(data)) { response in
+            
+            switch response.result {
+            case .success(let data):
+                if let JSON = data as? [String: AnyObject] {
+                    let records = JSON.valueForKeyPath(keyPath: "records")
+                    if let JSONArray: Array = records as? Array<[String: AnyObject]> {
+                        let dataObject = [Data].from(jsonArray: JSONArray)
+                        if dataObject != nil {
+                            completion(dataObject, response.result.error)
+                        } else {
+                            completion(nil, "Failed to parse JSON while conversion" as? Error)
+                        }
+                    } else {
+                        completion(nil, "Failed to parse JSON while conversion" as? Error)
+                    }
+                }
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+        
+    }
+}
+
