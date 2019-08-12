c0aae62df92a6f058ad36183729e1526d7249234
Vaishnavi <>
2019-08-13 01:12:54 +0800

    Unit Testing & UI Testing
    * Implemented UI Testing for navigation on tap
    * Implemented tests for asynchronous network connectivity

--- DemoSPHTests/DemoSPHTests.swift
+++ DemoSPHTests/DemoSPHTests.swift
@@ -10,15 +10,47 @@ import XCTest
 @testable import DemoSPH
 
 class DemoSPHTests: XCTestCase {
-
+    var sut : URLSession!
+    
     override func setUp() {
+        super.setUp()
+        sut = URLSession(configuration: .default)
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDown() {
+        sut = nil
+        super.tearDown()
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    // Asynchronous test: success fast, failure slow
+    func testValidCallToGetsHTTPStatusCode200() {
+        // given
+        let url = URL(string: "https://data.gov.sg/api/action/datastore_search?&limit=100&resource_id=a807b7ab-6cad-4aa6-87d0-e283a7353a0f")
+        // 1
+        let promise = expectation(description: "Status code: 200")
+        
+        // when
+        let dataTask = sut.dataTask(with: url!) { data, response, error in
+            // then
+            if let error = error {
+                XCTFail("Error: \(error.localizedDescription)")
+                return
+            } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if statusCode == 200 {
+                    // 2
+                    promise.fulfill()
+                } else {
+                    XCTFail("Status code: \(statusCode)")
+                }
+            }
+        }
+        dataTask.resume()
+        // 3
+        wait(for: [promise], timeout: 5)
+    }
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
--- DemoSPHUITests/DemoSPHUITests.swift
+++ DemoSPHUITests/DemoSPHUITests.swift
@@ -9,26 +9,19 @@
 import XCTest
 
 class DemoSPHUITests: XCTestCase {
-
+    var app: XCUIApplication!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testNavigationOnTap() {
+        XCUIApplication().tables/*@START_MENU_TOKEN@*/.staticTexts["2011"]/*[[".cells.staticTexts[\"2011\"]",".staticTexts[\"2011\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.navigationBars["2011 data consumption"].exists)
     }
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
 }
